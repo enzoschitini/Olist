e9be7332f3ee74d8484d28d44c625eb09e34a06e
enzoschitini <schitinienzo@mail.com>
2024-11-13 20:35:24 -0300

    Olist

---
+++ streamlit_application/olist.py
@@ -0,0 +1,187 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+
+import utils.general as all
+import utils.pedidos as pds
+import utils.produtos as pdt
+import utils.metrics as mtc
+import utils.geodata as geo
+import utils.dataset as dts
+import streamlit_application.utils.about as abt
+
+# App Developer:     Enzo Schitini -- Data Science
+# Date:              2 Outubro 2024 -- 22 Outubro 2024
+# https://www.notion.so/enzoschitini/Bozza-113b4721c6fd80f6b86cc94f63687086?pvs=4
+
+# 📅 Compiti
+# 🕗 
+# Organizzare la cartella, si prega di crearne un'altra per questioni di sicurezza ✅   
+
+# ⌛
+# 
+
+logo = 'https://raw.githubusercontent.com/enzoschitini/Data-Science-Portfolio/refs/heads/main/01%20An%C3%A1lise%20explorat%C3%B3ria%20e%20limpeza%20de%20dados/E-commerce%20Customer%20Data%20For%20Behavior%20Analysis/Image/olistlogo.png'
+icon_page = 'streamlit_application/img/icone.png'
+
+st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_title="Olist Insights", page_icon=icon_page)
+# Recupera a senha do session_state ou define como '0' inicialmente
+password = st.session_state.get('password', '0')
+
+@st.cache_data
+def load_data():
+    olist = pd.read_csv('streamlit_application/data/Brazilian E-Commerce Public Dataset by Olist.csv').drop(columns='Unnamed: 0')
+    return olist
+
+# Load the data
+olist = load_data() 
+
+def periodo(olist):
+    olist['order_purchase_timestamp'] = pd.to_datetime(olist['order_purchase_timestamp'])
+
+    # Get the min and max dates and convert them to 'date' format
+    max_data = olist['order_purchase_timestamp'].max().date()
+    min_data = olist['order_purchase_timestamp'].min().date()
+
+    # Date inputs in the sidebar, using 'date' format
+    data_iniziale = st.sidebar.date_input('Data inicial',
+                value=min_data,
+                min_value=min_data,
+                max_value=max_data)
+
+    data_finale = st.sidebar.date_input('Data final',
+                value=max_data,
+                min_value=min_data,
+                max_value=max_data)
+
+    # Filter the DataFrame based on the selected date range
+    olist = olist[(olist['order_purchase_timestamp'].dt.date >= data_iniziale) & (olist['order_purchase_timestamp'].dt.date <= data_finale)]
+    return olist
+
+def init():
+    # Impostazioni della barra laterale
+    st.sidebar.image(logo, width=150)
+    st.sidebar.write('# E-Commerce Dashboard')
+    st.sidebar.write('')  
+
+    # 1. Sidebar menu
+    with st.sidebar:
+        selected = option_menu(
+            menu_title="",  # required
+            options=["Métricas Gerais", "Pedidos", "Produtos", "Mapa", "Sobre"],  # required
+            icons=["grid", "box", "bag", "map", "book"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            styles={
+                "container": {"padding": "5!important", "background-color": "#f0f2f6"},
+                "icon": {"color": "black", "font-size": "25px"},  # Default icon color
+                "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
+                "nav-link-selected": {"background-color": "blue", "color": "white", "icon": "white"},  # Selected icon color to white
+            }
+        )
+
+    def try_except(funcao, pacote, error):
+        mtc.markdown("Ops!", "Desculpe, ocorreu um erro!",
+                        f"Verifique a função {funcao} do pacote {pacote}.py", '#F8F8FF')
+
+        st.write("""Ou fale com: [Enzo Schitini](https://www.linkedin.com/in/enzoschitini/)""")
+        with st.expander("Relatório do erro"):
+            st.write(error)
+
+    if selected == "Métricas Gerais":
+        st.image('streamlit_application/img/Commerce Illustrations/vctrly-business-illustrations-220-work-on-computer.png', width=200)
+        st.title(f"{selected} - Painel de controle Olist")
+        try:
+            all.general(olist)
+        except Exception as error:
+            try_except('all', 'general', error)
+
+
+    if selected == "Pedidos":
+        try:
+            pds.metricas_pedidos(olist)
+        except Exception as error:
+            try_except('metricas_pedidos', 'pedidos', error)
+
+
+    if selected == "Produtos":
+        try:
+            opcao = mtc.escolher_opcao_sidbar('Escolha como quer ver as categorias', ['Análise de uma categoria', 'Análise geral'])
+            pdt.metricas_produtos(olist, opcao)
+        except Exception as error:
+            try_except('metricas_produtos', 'produtos', error)
+    
+    if selected == "Mapa":
+        try:
+            #opcao = mtc.escolher_opcao_sidbar('Escolha como quer ver as categorias', ['Geral', 'Pontos de venda', 'Comparar regiões'])
+            geo.maps(olist)
+        except Exception as error:
+            try_except('maps', 'geo', error)
+    
+    if selected == "Data Base":
+        try:
+            dts.data_filter(olist)
+        except Exception as error:
+            try_except('data_filter', 'dst', error)
+        
+
+
+    if selected == "Sobre":
+        st.write(f"### {selected}")
+        abt.about(olist)
+
+
+    st.sidebar.write("""
+    ---
+    [Enzo Schitini](https://www.linkedin.com/in/enzoschitini/)
+    """)
+
+init()
+
+def senha():
+    # Se a senha ainda não for '1', exibe o texto e o campo de input
+    if password != "1234":
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.image(logo, width=200)
+            st.write('')
+            st.title('Bem Vindo(a) • E-Commerce Analysis')
+
+            st.write('####  Número da senha: 1234')
+            password = st.text_input("Insira o número da senha e clique enter:", key="password")
+
+        with col2:
+            st.image('streamlit_application/img/cover.png')
+        
+    else:
+        # Senha correta, exibe mensagem de sucesso
+        init()
+
+
+    # Create the checkbox
+    toggle = st.checkbox('Enable feature')
+
+    # Check the state of the checkbox
+    if toggle:
+        st.write("The feature is enabled.")
+    else:
+        st.write("The feature is disabled.")
+
+    # Inject custom CSS to change the color of the slider's bar
+    st.markdown(
+        """
+        <style>
+        /* Style for the slider track */
+        div[data-baseweb="slider"] > div > div {
+            background: #4561FF !important;  /* Set the color for the slider bar */
+        }
+        
+        /* Style for the slider thumb */
+        div[data-baseweb="slider"] > div > div > div {
+            background-color: #4561FF !important;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
---
+++ streamlit_application/utils/metrics.py
@@ -0,0 +1,417 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import seaborn as sns
+import locale
+
+# App Developer:     Enzo Schitini -- Data Science
+# Date:              2 Outubro 2024 -- 22 Outubro 2024
+
+@st.cache_data
+def formatar_numero_grande(numero):
+    if numero >= 1_000_000_000:
+        return f"{numero / 1_000_000_000:.1f} bilhões"
+    elif numero >= 1_000_000:
+        return f"{numero / 1_000_000:.1f} milhões"
+    elif numero >= 1_000:
+        return f"{numero / 1_000:.1f} mil"
+    else:
+        return str(numero)
+
+def formatar_timedelta_em_portugues(td):
+    # Extrair os dias, horas, minutos e segundos do Timedelta
+    dias = td.days
+    horas, resto = divmod(td.seconds, 3600)
+    minutos, segundos = divmod(resto, 60)
+    
+    # Montar a string no formato desejado
+    partes = []
+    resumo = []
+    if dias > 0:
+        partes.append(f"{dias} Dias")
+        resumo.append(f"{dias} Dias")
+    if horas > 0:
+        partes.append(f"{horas} Horas")
+    if minutos > 0:
+        partes.append(f"{minutos} Minutos")
+    if segundos > 0:
+        partes.append(f"{segundos} Segundos")
+    
+    return ' • '.join(partes), ''.join(resumo)
+
+def formatar_timedelta_em_numero(td):
+    # Extrair os dias, horas, minutos e segundos do Timedelta
+    dias = td.days
+    dias = int(dias)
+    horas, resto = divmod(td.seconds, 3600)
+    minutos, segundos = divmod(resto, 60)
+    
+    return dias
+
+def markdown(text_01, text02, text_03, color):
+    # Estilos personalizados usando HTML e CSS
+    st.markdown(f"""
+        <style>
+        .custom-box {{
+            border: 5px solid {color};
+            padding: 20px;
+            border-radius: 5px;
+            margin-bottom: 20px;
+        }}
+        .blue-bold {{
+            color: blue;
+            font-weight: bold;
+        }}
+        </style>
+        <div class="custom-box">
+            <h3><span class="blue-bold">{text_01}</span> {text02}</h3>
+            <h5>{text_03}</h5>
+        </div>
+        """, unsafe_allow_html=True)
+
+def markdown_pedidos(text_01, text02, text_03, text_04, text_05, text_06, color):
+    # Estilos personalizados usando HTML e CSS
+    st.markdown(f"""
+        <style>
+        .custom-box {{
+            border: 5px solid {color};
+            padding: 20px;
+            border-radius: 5px;
+            margin-bottom: 20px;
+        }}
+        .blue-bold {{
+            color: blue;
+            font-weight: bold;
+        }}
+        </style>
+        <div class="custom-box">
+            <h3><span class="blue-bold">{text_01}</span> {text02}</h3>
+            <h5>{text_03}</h5>
+            <h5>{text_04}</h5>
+            <h5>{text_05}</h5>
+            <h5>{text_06}</h5>
+        </div>
+        """, unsafe_allow_html=True)
+
+def multiselect(options, Selecione):
+    # Widget per selezionare le colonne
+    colonne_selezionate = st.multiselect(
+        Selecione,
+        options=options,
+        default=options[:2] 
+    )
+
+    return colonne_selezionate
+
+@st.cache_data
+def grafico(olist):
+    # Converter coluna de data para o formato datetime
+    olist['order_purchase_timestamp'] = pd.to_datetime(olist['order_purchase_timestamp'])
+
+    # Ordenar o DataFrame pela coluna de data
+    olist = olist.sort_values(by='order_purchase_timestamp')
+
+    # Criar o gráfico de linha usando Plotly
+    fig = px.line(olist, x='order_purchase_timestamp', y='price', title='Lucro ao Longo do Tempo')
+
+    # Exibir o gráfico no Streamlit
+    st.plotly_chart(fig)
+
+@st.cache_data
+def order_id(olist):
+    # Definir o locale para português (Brasil)
+    locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')  # Use 'pt_BR.UTF-8' para Linux/Mac e 'Portuguese_Brazil.1252' no Windows
+
+    # Converter a coluna de data para o formato datetime
+    olist['order_purchase_timestamp'] = pd.to_datetime(olist['order_purchase_timestamp'])
+
+    # Agrupar os dados por mês e contar o número de pedidos (order_id)
+    olist['month_year'] = olist['order_purchase_timestamp'].dt.to_period('M')  # Extrai ano-mês
+    orders_per_month = olist.groupby('month_year').size().reset_index(name='order_count')
+
+    # Converter o período para string no formato de mês em português
+    orders_per_month['month_year'] = orders_per_month['month_year'].dt.strftime('%B %Y')  # Nome completo do mês em português
+
+    # Criar o gráfico de linha usando Plotly
+    fig = px.bar(orders_per_month, x='month_year', y='order_count', title='Quantidade de Pedidos por Mês')
+
+    # Ajustar o layout do eixo x para exibir corretamente os nomes
+    fig.update_layout(xaxis_title='Mês', yaxis_title='Quantidade de Pedidos')
+
+    # Exibir o gráfico no Streamlit
+    st.plotly_chart(fig)
+
+@st.cache_data
+def boxplot(olist, col):
+    # Calcola i limiti per gli outliers usando IQR
+    Q1 = olist[col].quantile(0.25)
+    Q3 = olist[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    # Limiti per definire gli outliers
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    # Filtra i dati per escludere gli outliers
+    df_filtered = olist[(olist[col] >= lower_bound) & (olist[col] <= upper_bound)]
+
+    # Configura il grafico
+    plt.figure(figsize=(12, 12))
+
+    # Crea boxplot per le colonne numeriche senza outliers
+    sns.boxplot(data=df_filtered[[col]])
+
+    # Aggiungi titolo e etichette
+    plt.title('Boxplot (Sem Outliers)')
+    plt.ylabel('Valor')
+    #plt.xlabel('')
+
+    # Mostra il grafico
+    st.pyplot(plt)
+
+@st.cache_data
+def grafico_categoria(coluna: str, dataframe, titolo):
+    # Contar a frequência de cada categoria na coluna
+    coluna_dic = dataframe[coluna].value_counts().to_dict()
+    
+    # Criar o gráfico de barras
+    fig = px.bar(
+        x=list(coluna_dic.values()), 
+        y=list(coluna_dic.keys()), 
+        orientation='h',  # Coloca o gráfico na horizontal
+        title=titolo,
+        labels={'x': 'Frequência', 'y': coluna},  # Renomeia os eixos
+        color=list(coluna_dic.keys()),  # Colore as barras por categoria
+        color_discrete_sequence=px.colors.qualitative.Set2  # Melhora a paleta de cores
+    )
+    
+    # Ajuste da formatação do layout
+    fig.update_layout(
+        xaxis_title='Frequência',
+        yaxis_title=coluna,
+        title_x=0.5,  # Centraliza o título
+        template='plotly_white',  # Aplica um layout mais limpo
+        yaxis=dict(categoryorder='total ascending')  # Ordena as categorias pelo valor
+    )
+    
+    # Exibir o gráfico
+    st.plotly_chart(fig)
+
+def escolher_opcao(pergunta, opcoes):
+    resposta = st.selectbox(pergunta, opcoes)
+    return resposta
+
+def escolher_opcao_sidbar(pergunta, opcoes):
+    resposta = st.sidebar.selectbox(pergunta, opcoes)
+    return resposta
+
+# Avaliação das categorias com uma fórmula
+
+def avaliar_categoria(valor_total_vendas, total_vendas, nota_media, taxa_frete, valor_medio_frete, preco_medio):
+    # Cálculos dos componentes de desempenho
+    g1 = (valor_total_vendas / total_vendas) * nota_media
+    g2 = taxa_frete + valor_medio_frete
+    g3 = 1 / preco_medio
+
+    # Desempenho bruto
+    desempenho = g1 - g2 + g3
+
+    # Ajuste no normalizador com um P_max maior
+    P_min = 0  # Pontuação mínima
+    P_max = 2000  # Aumentado para refletir melhor os valores de desempenho
+
+    # Normalizando o valor para uma escala de 0 a 100
+    normalizador = ((desempenho - P_min) / (P_max - P_min)) * 100
+
+    return round(normalizador, 2)
+
+@st.cache_data
+def line_metrics_time(sales_data, categoria, title):
+    # Rename the column 'month/year_of_purchase' to 'year_month' if needed
+    sales_data.rename(columns={'month/year_of_purchase': 'year_month'}, inplace=True)
+
+    # Convert 'year_month' to datetime format, allowing pandas to infer the format  
+    sales_data['year_month'] = pd.to_datetime(sales_data['year_month'], format='mixed', errors='coerce')
+
+    # Check for any NaT values after conversion
+    if sales_data['year_month'].isna().sum() > 0:
+        st.write("Warning: Some dates couldn't be parsed and have been set to NaT.")
+
+    # Sort the data by 'year_month'
+    sales_data = sales_data.sort_values('year_month')
+
+    # Create the line plot
+    fig = px.line(sales_data, 
+                x='year_month', 
+                y=categoria, 
+                title=title,
+                labels={'year_month': 'Mês/Ano', categoria: f'Valor {categoria}'},
+                markers=True,
+                template='ygridoff') 
+    
+    # plotly_white plotly_dark plotly ggplot2 seaborn simple_white presentation xgridoff ygridoff none
+
+    # Update y-axis for currency format
+    fig.update_yaxes(tickprefix="")
+
+    # Optional: Add grid lines
+    fig.update_layout(xaxis=dict(showgrid=True), yaxis=dict(showgrid=True))
+
+    # Display in Streamlit
+    st.plotly_chart(fig)
+
+@st.cache_data
+def grafico_pizza_rank(olist, coluna, title, calcolo):
+    # Agrupar as categorias e somar o valor de pagamento
+    if calcolo == 'sum':
+        grouped = olist.groupby('product_category_name')[coluna].sum().reset_index()
+    if calcolo == 'count':
+        grouped = olist.groupby('product_category_name')[coluna].count().reset_index()
+
+    # Ordenar pela soma de payment_value
+    grouped = grouped.sort_values(by=coluna, ascending=False)
+
+    # Definir quantas categorias principais serão mantidas (exemplo: 5)
+    top_n = 6
+    top_categories = grouped[:top_n]
+    other_categories = grouped[top_n:]
+
+    # Calcular a soma dos "outros"
+    if calcolo == 'sum':
+        other_sum = other_categories[coluna].sum()
+    if calcolo == 'count':
+        other_sum = other_categories[coluna].count()
+
+    # Adicionar a categoria "outros"
+    other_row = pd.DataFrame({'product_category_name': ['outros'], coluna: [other_sum]})
+    top_categories = pd.concat([top_categories, other_row], ignore_index=True)
+
+    # Definir cores para as categorias
+    cores_marcadores = ["khaki", "MediumSeaGreen", "crimson", "limegreen", "tomato", "lightgrey"]
+
+    # Criar o gráfico de pizza do tipo donut
+    fig = go.Figure(data=go.Pie(labels=top_categories['product_category_name'],
+                                values=top_categories[coluna],
+                                marker_colors=cores_marcadores,
+                                hole=0.5,  # Buraco central
+                                pull=[0.10, 0.10, 0.10, 0.10, 0.10, 0.10]))  # Destaque na terceira categoria
+
+    # Rótulos fora da pizza com percentuais e nome da categoria
+    fig.update_traces(textposition="outside", textinfo="percent+label")
+
+    # Legenda e título
+    fig.update_layout(legend_title_text="Categorias de Produtos",
+                    legend=dict(orientation="h", xanchor="auto", x=0.5),
+                    title=title)
+
+    # Texto no centro do gráfico
+    fig.update_layout(annotations=[dict(text="Percentual", x=0.5, y=0.5, font_size=18, showarrow=False)])
+
+    # Exibir o gráfico
+    st.plotly_chart(fig)
+
+def espaco():
+    st.write('')
+    st.write('---')
+    st.write('')
+
+@st.cache_data
+def partes(olist, coluna):
+    olist['order_purchase_timestamp'] = pd.to_datetime(olist['order_purchase_timestamp'])
+    # Assuming olist DataFrame is already defined and 'order_purchase_timestamp' is in datetime format
+    # Extract year and month
+    olist['year_month'] = olist['order_purchase_timestamp'].dt.to_period('M')
+
+    # Calculate monthly statistics
+    monthly_stats = olist.groupby('year_month')[coluna].agg(['mean', lambda x: x.quantile(0.25), 
+                                                                lambda x: x.quantile(0.5), 
+                                                                lambda x: x.quantile(0.75)]).reset_index()
+    monthly_stats.columns = ['year_month', 'mean', '25%', '50%', '75%']
+
+    # Create the Plotly figure
+    fig = go.Figure()
+
+    # Add traces for mean and percentiles
+    fig.add_trace(go.Scatter(x=monthly_stats['year_month'].astype(str), 
+                            y=monthly_stats['mean'], 
+                            mode='lines+markers', 
+                            name='Média'))
+
+    fig.add_trace(go.Scatter(x=monthly_stats['year_month'].astype(str), 
+                            y=monthly_stats['25%'], 
+                            mode='lines+markers', 
+                            name='25%', 
+                            line=dict(dash='dash')))
+
+    fig.add_trace(go.Scatter(x=monthly_stats['year_month'].astype(str), 
+                            y=monthly_stats['50%'], 
+                            mode='lines+markers', 
+                            name='50%', 
+                            line=dict(dash='dash')))
+
+    fig.add_trace(go.Scatter(x=monthly_stats['year_month'].astype(str), 
+                            y=monthly_stats['75%'], 
+                            mode='lines+markers', 
+                            name='75%', 
+                            line=dict(dash='dash')))
+
+    # Update layout
+    fig.update_layout(title=f'Quartis:',
+                    xaxis_title='Meses',
+                    yaxis_title=coluna,
+                    xaxis_tickangle=-45,
+                    legend_title='Estatísticas',
+                    template='plotly_white')
+
+    # Show the figure
+    st.plotly_chart(fig)
+
+@st.cache_data
+def ordem(sales_data, dic):
+    # Rename the column 'month/year_of_purchase' to 'year_month' if needed
+    sales_data.rename(columns={'month/year_of_purchase': 'year_month'}, inplace=True)
+
+    # Convert 'year_month' to datetime format, allowing pandas to infer the format  
+    sales_data['year_month'] = pd.to_datetime(sales_data['year_month'], format='mixed', errors='coerce')
+
+    # Sort the data by 'year_month'
+    sales_data = sales_data.sort_values('year_month')
+    sales_data['month/year_of_purchase'] = sales_data['month_of_purchase'].astype(str) + '-' + sales_data['year_of_purchase'].astype(str)
+
+    sales_data = list(sales_data['month/year_of_purchase'].unique())
+
+    dic_ordinato = {key: dic.get(key, 0) for key in sales_data}
+
+    return sales_data, dic_ordinato
+
+@st.cache_data
+def novos(dataframe, col:str, grafico:bool):
+    ids_vistos = set()
+    dic_qtd_ano = {}
+    yers = dataframe['month/year_of_purchase'].unique()
+
+    for x in yers:
+        ids = set(dataframe[dataframe['month/year_of_purchase'] == x][col].unique().tolist())
+        difference = len(ids - ids_vistos)
+        
+        dic_qtd_ano[x] = difference
+        ids_vistos.update(ids)
+    
+    dic_qtd_ano = list(ordem(dataframe.copy(), dic_qtd_ano))[1]
+
+    if grafico == 0:
+        return dic_qtd_ano
+    elif grafico == 1:
+        # Create the bar plot
+        fig = px.bar(
+            x=dic_qtd_ano.keys(), 
+            y=dic_qtd_ano.values(), 
+            title='Análise da entrada de novos registros',
+            labels={'x': 'Mês/Ano', 'y': 'Quantidade'}, 
+            template='ygridoff'
+        )
+
+        fig.update_layout(xaxis=dict(showgrid=True), yaxis=dict(showgrid=True))
+        st.plotly_chart(fig)

35cd9d114073a1fd7faa6e86c8abb28bbb22e5c0
enzoschitini <schitinienzo@mail.com>
2024-11-13 20:47:49 -0300

    Olist

--- streamlit_application/olist.py
+++ streamlit_application/olist.py
@@ -22,7 +22,7 @@ import streamlit_application.utils.about as abt
 # 
 
 logo = 'https://raw.githubusercontent.com/enzoschitini/Data-Science-Portfolio/refs/heads/main/01%20An%C3%A1lise%20explorat%C3%B3ria%20e%20limpeza%20de%20dados/E-commerce%20Customer%20Data%20For%20Behavior%20Analysis/Image/olistlogo.png'
-icon_page = 'streamlit_application/img/icone.png'
+icon_page = 'https://raw.githubusercontent.com/enzoschitini/Olist/refs/heads/main/streamlit_application/img/Icone.png'
 
 st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_title="Olist Insights", page_icon=icon_page)
 # Recupera a senha do session_state ou define como '0' inicialmente

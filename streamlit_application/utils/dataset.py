6605dece7baa30d7a5fd4144e3aecae561ffb484
enzoschitini <schitinienzo@mail.com>
2024-11-14 07:56:21 -0300

    Olist

--- streamlit_application/utils/dataset.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def data_filter(olist):
-    st.image('streamlit_application/img/Commerce Illustrations/vctrly-business-illustrations-3-unboxing.png', width=150)
-    st.title(f"Análise dos Pedidos")
